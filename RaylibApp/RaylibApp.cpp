861747b9c45ee1b587b1dcce141900ca178e3cd6
Jack Brewer <jack_Brewer96@hotmail.com>
2025-05-19 17:30:52 +0100

    Added escape menu and started setup for different screens

--- RaylibApp/RaylibApp.cpp
+++ RaylibApp/RaylibApp.cpp
@@ -4,12 +4,14 @@
 #include "RaylibApp.h"
 #include "raylib.h"
 
+#define XBOX_ALIAS_1 "xbox"
+#define XBOX_ALIAS_2 "x-box"
+#define PS_ALIAS     "playstation"
+
 using namespace std;
 
 int main()
 {
-	
-
     // Initialization
     //--------------------------------------------------------------------------------------
     Log::Init();
@@ -17,16 +19,101 @@ int main()
     const int screenHeight = 320;
     InitWindow(screenWidth, screenHeight, "Chip8 Emulator");
 
+    SetExitKey(KEY_NULL);       // Disable KEY_ESCAPE to close window, X-button still works
+
+    bool exitWindowRequested = false;   // Flag to request window to exit
+    bool exitWindow = false;    // Flag to set window to exit
+
+    screens currentScreen = Logo;
+
+    int framesCounter = 0;
+
+    const float leftStickDeadzoneX = 0.1f;
+    const float leftStickDeadzoneY = 0.1f;
+    const float rightStickDeadzoneX = 0.1f;
+    const float rightStickDeadzoneY = 0.1f;
+    const float leftTriggerDeadzone = -0.9f;
+    const float rightTriggerDeadzone = -0.9f;
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!exitWindow)
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        // Detect if X-button or KEY_ESCAPE have been pressed to close window
+        if (WindowShouldClose() || IsKeyPressed(KEY_ESCAPE))
+        {
+            exitWindowRequested = true;
+        }
+
+        if (exitWindowRequested)
+        {
+            // A request for close window has been issued, we can save data before closing
+            // or just show a message asking for confirmation
+
+            if (IsKeyPressed(KEY_Y)) exitWindow = true;
+            else if (IsKeyPressed(KEY_N)) exitWindowRequested = false;
+        }
+
+        switch (currentScreen)
+        {
+            case Logo:
+            {
+                // TODO: Update LOGO screen variables here!
+
+                framesCounter++;    // Count frames
+
+                // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+                if (framesCounter > 120)
+                {
+                    currentScreen = Title;
+                }
+            } break;
+            case Menu:
+            {
+                // TODO: Update Menu screen variables here!
+
+                // Press enter to change to GAMEPLAY screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP) || IsGamepadButtonDown(0,GAMEPAD_BUTTON_RIGHT_FACE_LEFT))
+                {
+                    currentScreen = Title;
+                }
+            } break;
+            case Title:
+            {
+                // TODO: Update TITLE screen variables here!
+
+                // Press enter to change to GAMEPLAY screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_RIGHT_FACE_LEFT))
+                {
+                    currentScreen = Gameplay;
+                }
+            } break;
+            case Gameplay:
+            {
+                // TODO: Update GAMEPLAY screen variables here!
+
+                // Press enter to change to ENDING screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_RIGHT_FACE_LEFT))
+                {
+                    currentScreen = Ending;
+                }
+            } break;
+            case Ending:
+            {
+                // TODO: Update ENDING screen variables here!
+
+                // Press enter to return to TITLE screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_RIGHT_FACE_LEFT))
+                {
+                    currentScreen = Menu;
+                }
+            } break;
+            default: break;
+        }
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -34,25 +121,65 @@ int main()
 
         ClearBackground(RAYWHITE);
 
-        for (int i = 0; i < screenWidth / 10 + 1; i++)
+        switch (currentScreen)
         {
-            DrawLineV(Vector2 { (float)10 * i, 0 }, Vector2{ (float)10 * i, (float)screenHeight }, LIGHTGRAY);
-        }
-
-        for (int i = 0; i < screenHeight / 10 + 1; i++)
-        {
-            DrawLineV(Vector2{ 0, (float)10* i }, Vector2{ (float)screenWidth, (float)10 * i }, LIGHTGRAY);
-        }
-
-        /*for (int i = 0; i < screenWidth / 10; i++)
-        {
-            for (int j = 0; j < screenHeight / 10; j++)
+            case Logo:
             {
-                DrawText(TextFormat("[%i,%i]", i, j), 10 + 10 * i, 15 + 10 * j, 10, LIGHTGRAY);
-            }
-        }*/
+                // TODO: Draw LOGO screen here!
+                DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
+                DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
 
-        //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            } break;
+            case Menu:
+            {
+                // TODO: Draw Menu screen here!
+                DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+                DrawText("MENU SCREEN", 20, 20, 40, DARKGREEN);
+                DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+
+            } break;
+            case Title:
+            {
+                // TODO: Draw TITLE screen here!
+                DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+                DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+                DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+
+            } break;
+            case Gameplay:
+            {
+                for (int i = 0; i < screenWidth / 10 + 1; i++)
+                {
+                    DrawLineV(Vector2{ (float)10 * i, 0 }, Vector2{ (float)10 * i, (float)screenHeight }, LIGHTGRAY);
+                }
+
+                for (int i = 0; i < screenHeight / 10 + 1; i++)
+                {
+                    DrawLineV(Vector2{ 0, (float)10 * i }, Vector2{ (float)screenWidth, (float)10 * i }, LIGHTGRAY);
+                }
+                DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
+                DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+
+            } break;
+            case Ending:
+            {
+                // TODO: Draw ENDING screen here!
+                DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+                DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+
+            } break;
+            default:
+            {
+                break;
+            }
+
+            if (exitWindowRequested)
+            {
+                DrawRectangle(0, screenHeight / 2, screenWidth, screenHeight / 4, BLACK);
+                DrawText("Are you sure you want to exit program? [Y/N]", 40, screenHeight / 2, 15, WHITE);
+            }
+        }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
--- RaylibApp/RaylibApp.h
+++ RaylibApp/RaylibApp.h
@@ -5,4 +5,12 @@
 
 #include <iostream>
 
+enum screens
+{
+	Logo = 0,
+	Menu = 1,
+	Title = 2,
+	Gameplay = 3,
+	Ending = 4
+};
 // TODO: Reference additional headers your program requires here.
